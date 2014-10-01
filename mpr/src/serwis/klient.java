b6c5261dbdefab179e98f3a00e9ad7ac97558fc1
s11645 <s11645@pjwstk.edu.pl>
2014-10-01 19:54:47 +0200

    klient

---
+++ mpr/src/serwis/klient.java
@@ -0,0 +1,39 @@
+package serwis;
+
+public class klient 
+{
+private String imie;
+private String nazwisko;
+private String kontobankowe;
+
+public String getImie() 
+{
+	return imie;
+}
+
+public void setImie(String imie) 
+{
+	this.imie = imie;
+}
+
+public String getNazwisko() 
+{
+	return nazwisko;
+}
+
+public void setNazwisko(String nazwisko) 
+{
+	this.nazwisko = nazwisko;
+}
+
+public String getKontobankowe() 
+{
+	return kontobankowe;
+}
+
+public void setKontobankowe(String kontobankowe) 
+{
+	this.kontobankowe = kontobankowe;
+}
+
+}
