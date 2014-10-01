a04c52d65f2ad7bf5ea746ee8978f9931af2d822
s11645 <s11645@pjwstk.edu.pl>
2014-10-01 20:06:51 +0200

    sprzęt i zamówienie

---
+++ mpr/src/serwis/sprzet.java
@@ -0,0 +1,50 @@
+package serwis;
+
+public class sprzet 
+{
+	private String producent;
+	private String model;
+	private String usterka;
+	private String typ;
+	
+	public String getProducent() 
+	{
+		return producent;
+	}
+	
+	public void setProducent(String producent) 
+	{
+		this.producent = producent;
+	}
+	
+	public String getModel() 
+	{
+		return model;
+	}
+	
+	public void setModel(String model) 
+	{
+		this.model = model;
+	}
+	
+	public String getUsterka()
+	{
+		return usterka;
+	}
+	
+	public void setUsterka(String usterka) 
+	{
+		this.usterka = usterka;
+	}
+	
+	public String getTyp() 
+	{
+		return typ;
+	}
+	
+	public void setTyp(String typ) 
+	{
+		this.typ = typ;
+	}
+	
+}
---
+++ mpr/src/serwis/zamowienie.java
@@ -0,0 +1,38 @@
+package serwis;
+
+public class zamowienie 
+{
+	private float kwota;
+	private String dataprzyjecia;
+	private String datawykonania;
+	
+	public float getKwota() 
+	{
+		return kwota;
+	}
+	
+	public void setKwota(float kwota)
+	{
+		this.kwota = kwota;
+	}
+	
+	public String getDataprzyjecia() 
+	{
+		return dataprzyjecia;
+	}
+	public void setDataprzyjecia(String dataprzyjecia) 
+	{
+		this.dataprzyjecia = dataprzyjecia;
+	}
+	
+	public String getDatawykonania() 
+	{
+		return datawykonania;
+	}
+	
+	public void setDatawykonania(String datawykonania) 
+	{
+		this.datawykonania = datawykonania;
+	}
+	
+}
